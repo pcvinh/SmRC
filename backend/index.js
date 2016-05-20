6a89334d214f839bc533f5bfee76a39aae5f380d
samrove <samrove97@gmail.com>
2016-05-20 11:26:29 +0800

    first commit for backend

---
+++ backend/index.js
@@ -0,0 +1,76 @@
+var app = require('express')();
+var bodyParser = require('body-parser');
+
+
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/starhub';
+
+var db;
+
+app.use(bodyParser.json({strict: false})); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing 
+
+// Initialize connection once
+MongoClient.connect(url, function(err, database) {
+  if(err) throw err;
+ 
+  db = database;
+
+  // Start the application after the database connection is ready
+  app.listen(3000);
+  console.log("Listening on port 3000");
+});
+
+var randomString = function(length) {
+    var text = "";
+    var possible = "0123456789";
+    for(var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+
+
+// http://<ip>:<port>/PairingInit?ChannelId&STBName
+app.get('/PairingInit', function (req, res) {
+  var channelId= req.query.ChannelId, STBName = req.query.STBName;
+  var rs = randomString(6);  
+
+	db.collection("testCollection").insert({
+		
+		ChannelId : channelId,
+		STBName : STBName,
+		OTP : rs
+		
+	});
+  res.send(rs); 
+})
+// http://<ip>:<port>/Pairing?ChannelId&STBName
+app.post('/Pairing', function (req, res) {
+  var rs = req.body.OTP, user = req.body.Username;
+  
+  var collection = db.collection("testCollection");
+  if(user == null || user == undefined) {
+	  user = "Anonymous";
+  }
+  
+   collection.findAndModify(
+   {OTP: rs}, //query
+   {$set: {username : user}}, //update statement
+   {new: true}, // modified document
+   {}, //options
+	function (err , item) {
+	   
+	  var channelId = item.channelId, stbname = item.STBName;
+		  
+	  var jwt = require('jsonwebtoken');
+	  var token = jwt.sign({ ChannelId: channelid });
+	  
+	  res.jsonp({token : token, stb : stbname}); 
+	  
+	 
+	})
+
+})
+
